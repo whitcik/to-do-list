fa408574cad2ccc1fbec6ff334b67859a9d16d1d
whitcik <whitcik@gmail.com>
2017-09-02 22:06:33 +0200

    Added component Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from 'components/Header/Header.js'
 import Content from 'components/Content/Content.js'
+import Footer from 'components/Footer/Footer.js'
 import './App.scss';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Header title="To Do List" />
         <Content />
+        <Footer />
       </div>
     );
   }
